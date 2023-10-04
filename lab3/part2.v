fe59c8b952784b99bafbb132e243d482dfe0a469
Ashley Leal <anleal2004@gmail.com>
2023-10-04 11:36:28 -0400

    NEW

--- lab3/part2.v
+++ lab3/part2.v
@@ -1,20 +1,26 @@
 /* Lab 3 Part 2 */
 
-module part2( input [3:0] a, input [3:0] b, input Function, output [7:0] ALUout );
+// Map to FPGA
+module part2(input [8:0] SW, output [9:0] LEDR);
+
+    // Instantiate MUX
+    mux alu( .a(SW[]), .b(SW[]), .Function(), .ALUout())
+
+endmodule
+
+// ALU Module
+module mux( input [3:0] a, input [3:0] b, input Function, output [7:0] ALUout );
 
     // Instantiate each function outside of the always block
     fourBitRippleCarryAdder function0 (.a(), .b(), .c_in(), .c_out(), .s());
-    singleReductionOR function1 (.a(), .b(), .ab());
-    singleReductionAND function2 (.a(), .b(), .ab());
-    concatenation function3 (.a(), .b(), .ab());
 
     always @ (a, b) begin
         case(Function)
 
         2'b00 : assign ALUout = function0 (); // output sum of RCA
-        2'b01 : assign ALUout = function1 (); // output 8'b00000001 if a or b
-        2'b10 : assign ALUout = function2 (); // output 8'b00000001 if a and b
-        3'b11 : assign ALUout = function3 (); // output a concat b
+        2'b01 : assign ALUout = a | b ? 8'b00000001 : 8'b00000000; // output 8'b00000001 if a or b
+        2'b10 : assign ALUout = a & b ? 8'b00000001 : 8'b00000000; // output 8'b00000001 if a and b
+        3'b11 : assign ALUout = {a, b}; // output a concat b
 
         endcase
     end
@@ -41,27 +47,3 @@ module fourBitRippleCarryAdder( input [3:0] a, input [3:0] b, input c_in, output
     fullAdder bit3 (.c_in(c[2]), .a(a[3]), .b(b[3]), .c_out(c_out), .s(s[3]));
 	
 endmodule // fourBitRippleCarryAdder
-
-// Function 1: Single reduction (bitwise) OR
-
-module singleReductionOR ( input [3:0] a, input [3:0] b, output [7:0] ab);
-    
-    assign ab = a | b ? 8'b00000001;
-
-endmodule
-
-// Function 2: Single reduction (bitwise) AND
-
-module singleReductionAND ( input [3:0] a, input [3:0] b, output [7:0] ab);
-    
-    assign ab = a & b ? 8'b00000001;
-
-endmodule
-
-// Function 3: A most significant 4 bits, B lower 4 bits
-
-module concatenation( input [3:0] a, input [3:0] b, output [7:0] ab);
-
-    assign ab = {a, b};
-
-endmodule
