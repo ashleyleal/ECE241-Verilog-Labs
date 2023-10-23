03e2f570fd94e9fd060a38a79bea7b4a26803398
chocoboash <anleal2004@gmail.com>
2023-10-23 00:55:48 -0400

    formatted

--- lab5/part2.v
+++ lab5/part2.v
@@ -1,48 +1,52 @@
 // Top Level Module
-module part2
-#(parameter CLOCK_FREQUENCY = 50000000)(
-input ClockIn,
-input Reset,
-input [1:0] Speed,
-output [3:0] CounterValue
+module part2 #(
+    parameter CLOCK_FREQUENCY = 50000000
+) (
+    input ClockIn,
+    input Reset,
+    input [1:0] Speed,
+    output [3:0] CounterValue
 );
 
-reg EnableDC;
+  reg EnableDC;
 
 
-// Instantiate Rate Divider
-  RateDivider #(.CLOCK_FREQUENCY(CLOCK_FREQUENCY)) RDInst (
-    .ClockIn(ClockIn),
-    .Reset(Reset),
-    .Speed(Speed),
-    .Enable(EnableDC)
+  // Instantiate Rate Divider
+  RateDivider #(
+      .CLOCK_FREQUENCY(CLOCK_FREQUENCY)
+  ) RDInst (
+      .ClockIn(ClockIn),
+      .Reset  (Reset),
+      .Speed  (Speed),
+      .Enable (EnableDC)
   );
 
   // Instantiate Display Counter
   DisplayCounter DCInst (
-    .Clock(ClockIn),
-    .Reset(Reset),
-    .EnableDC(EnableDC),
-    .CounterValue(CounterValue)
+      .Clock(ClockIn),
+      .Reset(Reset),
+      .EnableDC(EnableDC),
+      .CounterValue(CounterValue)
   );
 
 endmodule
 
 // Rate Divider
-module RateDivider
-#(parameter CLOCK_FREQUENCY = 50000000) (
-input ClockIn,
-input Reset,
-input [1:0] Speed,
-output Enable
+module RateDivider #(
+    parameter CLOCK_FREQUENCY = 50000000
+) (
+    input ClockIn,
+    input Reset,
+    input [1:0] Speed,
+    output Enable
 );
 endmodule
 
 // Display Counter
 module DisplayCounter (
-input Clock,
-input Reset,
-input EnableDC,
-output [3:0] CounterValue
+    input Clock,
+    input Reset,
+    input EnableDC,
+    output [3:0] CounterValue
 );
 endmodule
