abd928bd81b02bb076435e0f59453d42e2acde34
chocoboash <anleal2004@gmail.com>
2023-10-23 00:52:51 -0400

    added do files and module declarations

--- lab5/part2.v
+++ lab5/part2.v
@@ -1,3 +1,4 @@
+// Top Level Module
 module part2
 #(parameter CLOCK_FREQUENCY = 50000000)(
 input ClockIn,
@@ -5,3 +6,43 @@ input Reset,
 input [1:0] Speed,
 output [3:0] CounterValue
 );
+
+reg EnableDC;
+
+
+// Instantiate Rate Divider
+  RateDivider #(.CLOCK_FREQUENCY(CLOCK_FREQUENCY)) RDInst (
+    .ClockIn(ClockIn),
+    .Reset(Reset),
+    .Speed(Speed),
+    .Enable(EnableDC)
+  );
+
+  // Instantiate Display Counter
+  DisplayCounter DCInst (
+    .Clock(ClockIn),
+    .Reset(Reset),
+    .EnableDC(EnableDC),
+    .CounterValue(CounterValue)
+  );
+
+endmodule
+
+// Rate Divider
+module RateDivider
+#(parameter CLOCK_FREQUENCY = 50000000) (
+input ClockIn,
+input Reset,
+input [1:0] Speed,
+output Enable
+);
+endmodule
+
+// Display Counter
+module DisplayCounter (
+input Clock,
+input Reset,
+input EnableDC,
+output [3:0] CounterValue
+);
+endmodule
