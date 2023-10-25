c07894dc08ba18e2295f7ebe167159c8003b0b30
chocoboash <anleal2004@gmail.com>
2023-10-24 22:25:44 -0400

    ttff

--- lab5/part1.v
+++ lab5/part1.v
@@ -1,1 +1,19 @@
 module part1 (input Clock, input Enable, input Reset, output [7:0] CounterValue);
+
+endmodule
+
+
+module TFlipFlop (
+    input wire T,
+    input wire Clock,
+    input wire Reset,
+    output reg q
+);
+    always @(posedge Clock)
+        begin
+        if (!Reset)
+            q <= 0;
+        else if (T)
+            Q <= ~Q;
+        end
+endmodule
