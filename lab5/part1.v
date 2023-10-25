ec7eafaa6382392ca73c5d169045e72417710381
chocoboash <anleal2004@gmail.com>
2023-10-24 22:38:52 -0400

    ???????????????????

--- lab5/part1.v
+++ lab5/part1.v
@@ -1,65 +1,69 @@
 module part1 (input Clock, input Enable, input Reset, output [7:0] CounterValue);
 
     wire t0, t1, t2, t3, t4, t5, t6, t7;
-    reg [7:0] counter_values;
+    reg [7:0] counters;
     
+    assign t0 = Enable;
+
     TFlipFlop tff0 (
         .T(t0),
         .Clock(Clock),
         .Reset(Reset),
-        .q(counter_values[0])
+        .q(counters[0])
     );
+
+    assign t1 = Enable & counters[0];
     
     TFlipFlop tff1 (
         .T(t1),
         .Clock(Clock),
         .Reset(Reset),
-        .q(counter_values[1])
+        .q(counters[1])
     );
     
     TFlipFlop tff2 (
         .T(t2),
         .Clock(Clock),
         .Reset(Reset),
-        .q(counter_values[2])
+        .q(counters[2])
     );
     
     TFlipFlop tff3 (
         .T(t3),
         .Clock(Clock),
         .Reset(Reset),
-        .q(counter_values[3])
+        .q(counters[3])
     );
     
     TFlipFlop tff4 (
         .T(t4),
         .Clock(Clock),
         .Reset(Reset),
-        .q(counter_values[4])
+        .q(counters[4])
     );
     
     TFlipFlop tff5 (
         .T(t5),
         .Clock(Clock),
         .Reset(Reset),
-        .q(counter_values[5])
+        .q(counters[5])
     );
     
     TFlipFlop tff6 (
         .T(t6),
         .Clock(Clock),
         .Reset(Reset),
-        .q(counter_values[6])
+        .q(counters[6])
     );
     
     TFlipFlop tff7 (
         .T(t7),
         .Clock(Clock),
         .Reset(Reset),
-        .q(counter_values[7])
+        .q(counters[7])
     );
     
-    assign CounterValue = counter_values;
+    assign CounterValue = counters;
     
 endmodule
 
@@ -74,6 +78,6 @@ module TFlipFlop (
         if (!Reset)
             q <= 0;
         else if (T)
-            Q <= ~Q;
+            q <= ~q;
         end
 endmodule
